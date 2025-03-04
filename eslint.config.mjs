e6e3d6cefab4d9b0c22a42fd4a28eb1e3fdd1df1
priscillacs <pcsetia03@gmail.com>
2025-03-04 22:22:07 +0800

    fix error eslint_3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+export default [
   {
-    plugins: ["@typescript-eslint"], // Add this line
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    parser: typescriptParser,
+    extends: ["plugin:@next/next/recommended"],
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
         },
       ],
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
-      "react-hooks/rules-of-hooks": "warn",
-      "@next/next/no-img-element": "warn",
     },
   },
 ];
-
-export default eslintConfig;
